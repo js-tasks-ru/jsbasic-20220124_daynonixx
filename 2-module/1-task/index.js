3f9a5094d5735c3859c86536df6484f88192a21e
Daniil Petrishin <daynonixx@gmail.com>
2022-02-02 18:35:43 +0200

    Module 2 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let item in salaries){
+    if (helper(salaries[item])){
+      sum += salaries[item];
+    }
+  }
+  return sum;
+}
+function helper(item){
+  return (typeof item === 'number' && item % 1 === 0 && item !== Number.POSITIVE_INFINITY && item !== Number.NEGATIVE_INFINITY);
 }
