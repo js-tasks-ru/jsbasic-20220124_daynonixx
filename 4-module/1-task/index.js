63b194ff00aa316eefb3fe323060142b214f777b
Daniil Petrishin <daynonixx@gmail.com>
2022-02-10 14:55:51 +0200

    Module 4 Exercises

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement('ul');
+
+  for (let friend of friends) {
+    list.innerHTML += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+  return list;
 }
